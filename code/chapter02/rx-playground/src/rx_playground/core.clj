3f96928428ffc0a82bc2d02ad3097b38de5c7d46
Leonardo Borges <leonardoborges.rj@gmail.com>
2015-02-16 22:15:57 +1100

    Add new examples covering Rx error handling and backpressure

--- code/chapter02/rx-playground/src/rx_playground/core.clj
+++ code/chapter02/rx-playground/src/rx_playground/core.clj
@@ -109,3 +109,137 @@
                    (rx/flatmap repeat-obs)
                    (rx/take 6))
               prn-to-repl)
+
+
+
+;;
+;; Error handling
+;;
+
+;; onError
+(defn exceptional-obs []
+  (rx/observable*
+   (fn [observer]
+     (rx/on-next observer (throw (Exception. "Oops. Something went wrong")))
+     (rx/on-completed observer))))
+
+(rx/subscribe (->> (exceptional-obs)
+                   (rx/map inc))
+              (fn [v] (prn-to-repl "result is " v)))
+;; Exception Oops. Something went wrong  rx-playground.core/exceptional-obs/fn--1505
+
+(rx/subscribe (->> (exceptional-obs)
+                   (rx/map inc))
+              (fn [v] (prn-to-repl "result is " v))
+              (fn [e] (prn-to-repl "error is " e)))
+
+;; "error is " #<Exception java.lang.Exception: Oops. Something went wrong>
+
+
+;; catch
+(rx/subscribe (->> (exceptional-obs)
+                   (rx/catch Exception e
+                       (rx/return 10))
+                   (rx/map inc))
+              (fn [v] (prn-to-repl "result is " v)))
+
+;; "result is " 11
+
+(rx/subscribe (->> (exceptional-obs)
+                   (rx/catch Exception e
+                     (rx/seq->o (range 5)))
+                   (rx/map inc))
+              (fn [v] (prn-to-repl "result is " v)))
+
+;; "result is " 1
+;; "result is " 2
+;; "result is " 3
+;; "result is " 4
+;; "result is " 5
+
+;; retry
+(defn retry-obs []
+  (let [errored (atom false)]
+    (rx/observable*
+     (fn [observer]
+       (throw (Exception. "Oops. Something went wrong"))))))
+
+(rx/subscribe (retry-obs)
+              (fn [v] (prn-to-repl "result is " v)))
+
+;; Exception Oops. Something went wrong  rx-playground.core/retry-obs/fn--1476
+
+(rx/subscribe (->> (retry-obs)
+                   (.retry))
+              (fn [v] (prn-to-repl "result is " v)))
+
+;; "result is " 20
+
+;;
+;; Backpressure
+;;
+
+
+(defn fast-producing-obs []
+  (rx/map inc (Observable/interval 1 TimeUnit/MILLISECONDS)))
+
+(defn slow-producing-obs []
+  (rx/map inc (Observable/interval 500 TimeUnit/MILLISECONDS)))
+
+(rx/subscribe (->> (rx/map vector
+                           (fast-producing-obs)
+                           (slow-producing-obs))
+                   (rx/map (fn [[x y]]
+                             (+ x y)))
+                   (rx/take 10))
+              prn-to-repl
+              (fn [e] (prn-to-repl "error is " e)))
+
+;; "error is " #<MissingBackpressureException rx.exceptions.MissingBackpressureException>
+
+;; throttle
+
+(rx/subscribe (->> (rx/map vector
+                           (.sample (fast-producing-obs) 200
+                                    TimeUnit/MILLISECONDS)
+                           (slow-producing-obs))
+                   (rx/map (fn [[x y]]
+                             (+ x y)))
+                   (rx/take 10))
+              prn-to-repl
+              (fn [e] (prn-to-repl "error is " e)))
+
+;; 204
+;; 404
+;; 604
+;; 807
+;; 1010
+;; 1206
+;; 1407
+;; 1613
+;; 1813
+;; 2012
+
+
+;; onBackpressureBuffer
+
+
+(rx/subscribe (->> (rx/map vector
+                           (.onBackpressureBuffer (fast-producing-obs))
+                           (slow-producing-obs))
+                   (rx/map (fn [[x y]]
+                             (+ x y)))
+                   (rx/take 10))
+              prn-to-repl
+              (fn [e] (prn-to-repl "error is " e)))
+
+;; 2
+;; 4
+;; 6
+;; 8
+;; 10
+;; 12
+;; 14
+;; 16
+;; 18
+;; 20
